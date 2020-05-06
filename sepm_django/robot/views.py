9c01640a3ec7df7b405cc9bf15c372bf39c134be
Omar Adnan <s3721552@student.rmit.edu.au>
2020-05-06 15:50:02 +1000

    template inheritence created

--- sepm_django/robot/views.py
+++ sepm_django/robot/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+tour_planner = [
+	{
+		'name_location': 'Bali',
+		'xy_coordinates': '8 S, 115 E',
+		'description': 'one stop trip(temporary)',
+		'min_time_spent': '2 days'
+
+	},
+	{
+		'name_location': 'Sydney',
+		'xy_coordinates': '33 S, 151 E',
+		'description': 'one stop trip(temporary)',
+		'min_time_spent': '4 days'
+
+	}
+]
+
 def home(request):
-	return HttpResponse('<h1>Home Page</h1>')
+	context = {
+		'tour_planner': tour_planner
+	}
+	return render(request, 'robot/home.html', context)
 
 def tours(request):
-	return HttpResponse('<h1>Tours Page</h1>')
+	return render(request, 'robot/tours.html', {'title': 'Tours'})
--- sepm_django/sepm_django/settings.py
+++ sepm_django/sepm_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'robot.apps.RobotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
